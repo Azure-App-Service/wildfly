454be9cece544e17862bd4ea600222f3aafd25d6
Shrirang Shirodkar <shrirs@microsoft.com>
2019-04-24 16:23:44 -0700

    Re-organize directory structure

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,9 +1,42 @@
-$env:wildflyVersion='14.0.1.Final'
-
-$path = ".\tmp"
-If(!(test-path $path))
+function setup
 {
-      New-Item -ItemType Directory -Force -Path $path
+	param([string]$version)
+
+    $tmpDirRootPath = $version + '/tmp'
+
+    # Copy the shared files to the target directory
+    copy-item -Force -recurse shared "$tmpDirRootpath/shared"
+    
+    $dockerFileTemplatePath = '.\shared\misc\Dockerfile'
+    $dockerFileOutPath = "$version\Dockerfile"
+
+    # Generate the Dockerfile from the template and place it in the target directory
+    # Also, copy Tomcat version specific files to the target directory
+    switch ($version)
+    {
+        '14-jre8'
+        {
+            $wildflyMajorVersion = '14'
+            $wildflyVersion = '14.0.1.Final'
+
+            $content = ((Get-Content -path $dockerFileTemplatePath -Raw) `
+                -replace '__PLACEHOLDER_BASEIMAGE__', 'mcr.microsoft.com/java/jre-headless:8u212-zulu-alpine-with-tools') `
+                -replace '__PLACEHOLDER_WILDFLY_MAJOR_VERSION__', $wildflyMajorVersion `
+                -replace '__PLACEHOLDER_WILDFLY_VERSION__', $wildflyVersion
+            
+            break
+        }
+    }
+
+    $wildflyLocalPath = "$tmpDirRootPath\wildfly-$wildflyVersion.tar.gz"
+    If(!(test-path $wildflyLocalPath))
+    {
+        Invoke-WebRequest -Uri https://download.jboss.org/wildfly/$wildflyVersion/wildfly-$wildflyVersion.tar.gz -OutFile $wildflyLocalPath
+    }
+
+    $headerFooter = "########################################################`n### ***DO NOT EDIT*** This is an auto-generated file ###`n########################################################`n"
+    $content = $headerFooter + $content + $headerFooter
+    Set-Content -Value $content -Path $dockerFileOutPath
 }
 
-Invoke-WebRequest -Uri https://download.jboss.org/wildfly/$env:wildflyVersion/wildfly-$env:wildflyVersion.tar.gz -OutFile .\tmp\wildfly-$env:wildflyVersion.tar.gz
+setup -version '14-jre8'
